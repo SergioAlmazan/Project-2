599f5206401ed0a491ea0c253089908f986c7eac
BPM <brantleymorris@me.com>
2021-01-29 11:54:23 -0600

    add routes to quiz-api

--- routes/quiz-api-routes.js
+++ routes/quiz-api-routes.js
@@ -3,8 +3,27 @@
 var db = require("../models");
 
 module.exports = function(app) {
+
+    // get all quizes
+    app.get("/api/quizes", function(req, res) {
+        db.quiz.findAll({
+            include: [db.score, db.quesiton]
+        });
+    });
+
+    // gets all quizes with the given subject
+    app.get("/api/quizes/:subject", function(req, res) {
+        db.quiz.findAll({
+            where: {
+                subject: req.params.subject
+            },
+            include: [db.score, db.question]
+        }).then(function(dbQuiz) {
+            res.json(dbQuiz);
+        });
+    });
     
-    // get all quiz information by quiz name
+    // get quiz information by quiz name
     // used for displaying data to quiz home page
     app.get("/api/quizes/:name", function(req, res) {
         db.quiz.findAll({
