8d53fbf74f9ac3784eb03d61459590efa5ec183a
Daniel Reza <daniel.reza1007@gmail.com>
2021-02-01 19:55:21 -0600

    test commit

--- public/js/login.js
+++ public/js/login.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var usernameInput = $("input#username-input");
     var passwordInput = $("input#password-input");
   
-    // When the form is submitted, we validate there's an email and password entered
+    // When the form is submitted, we validate there's an username and password entered
     loginForm.on("submit", function(event) {
       event.preventDefault();
       var userData = {
@@ -16,7 +16,7 @@ $(document).ready(function() {
         return;
       }
   
-      // If we have an email and password we run the loginUser function and clear the form
+      // If we have an username and password we run the loginUser function and clear the form
       loginUser(userData.username, userData.password);
       usernameInput.val("");
       passwordInput.val("");
@@ -37,23 +37,50 @@ $(document).ready(function() {
         });
     }
 
-    // If we have an email and password we run the loginUser function and clear the form
-    loginUser(userData.username, userData.password);
-    usernameInput.val("");
-    passwordInput.val("");
   });
-
-  // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-  function loginUser(username, password) {
-    $.post("/api/login", {
-      username: username,
-      password: password
-    })
-      .then(function () {
-        window.location.replace("/user");
-        // If there's an error, log the error
+  
+  $(document).ready(function() {
+    // Getting references to our form and input
+    var signUpForm = $("form.signup");
+    var usernameInput = $("input#username-input1");
+    var passwordInput = $("input#password-input1");
+  
+    // When the signup button is clicked, we validate the username and password are not blank
+    signUpForm.on("submit", function(event) {
+      event.preventDefault();
+      var userData = {
+        username: usernameInput.val().trim(),
+        password: passwordInput.val().trim()
+      };
+  
+      if (!userData.username || !userData.password) {
+        return;
+      }
+      // If we have an username and password, run the signUpUser function
+      // eslint-disable-next-line no-use-before-define
+      signUpUser(userData.username, userData.password);
+      usernameInput.val("");
+      passwordInput.val("");
+    });
+  
+    // Does a post to the signup route. If successful, we are redirected to the members page
+    // Otherwise we log any errors
+    function signUpUser(username, password) {
+      $.post("/api/signup", {
+        username: username,
+        password: password
       })
-      .catch(function(err) {
-        console.log(err);
-      });
-  }
+        // eslint-disable-next-line no-unused-vars
+        .then(function(data) {
+          window.location.replace("/user");
+          // If there's an error, handle it by throwing up a bootstrap alert
+        })
+        // eslint-disable-next-line no-use-before-define
+        .catch(handleLoginErr);
+    }
+  
+    function handleLoginErr(err) {
+      $("#alert .msg").text(err.responseJSON);
+      $("#alert").fadeIn(500);
+    }
+  });
